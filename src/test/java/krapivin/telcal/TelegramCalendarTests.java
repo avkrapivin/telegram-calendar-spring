43994fe621ebcea93c7fef464be5c8870b33d4a1
Андрей Крапивин <without.a.body@gmail.com>
2024-11-20 20:17:21 +0200

    Base template for spring webhook

---
+++ src/test/java/krapivin/telcal/TelegramCalendarTests.java
@@ -0,0 +1,13 @@
+package krapivin.telcal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramCalendarTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
